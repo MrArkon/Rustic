8e953fcd71064b5158267d370265b910a8981cb6
MrArkon <mrarkon@outlook.com>
2021-10-17 11:10:37 +0530

    🗃 Update migrations

--- migrations/20211016055959_rustic.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS guilds
-(
-    guild_id BIGINT PRIMARY KEY,
-    prefix   VARCHAR NOT NULL
-);
---
+++ migrations/20211017053519_rustic.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS guilds
+(
+    guild_id BIGINT NOT NULL PRIMARY KEY,
+    prefix   TEXT
+);
